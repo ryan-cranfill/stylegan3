98e266a6dedaad6e2e34f16453dd9b5c38d5bf46
Ryan Cranfill <ryanccranfill@gmail.com>
2021-10-14 12:20:48 -0500

    add setup.py, add requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stylegan3',
+    version='',
+    packages=['viz', 'dnnlib', 'metrics', 'training', 'gui_utils', 'torch_utils', 'torch_utils.ops'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
